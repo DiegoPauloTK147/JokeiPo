a438ef8ea499c7b69ca1c1187d0e4cd53db05546
DiegoPauloTK147 <editordiegopaulo@gmail.com>
2025-05-02 15:51:13 -0300

    Finalizado

--- jokenposcript.js
+++ jokenposcript.js
@@ -9,6 +9,17 @@ const scissorsMachine = document.getElementById('scissors-machine')
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+const GAME_OPTIONS = {
+     ROCK: 'rock',
+     PAPER: 'paper',
+     SCISSORS: 'scissors'
+}
+
+const COLOR_OPTIONS = {
+    RED: 'red',
+    BLACK: 'black'
+}
+
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
@@ -16,7 +27,7 @@ const playHuman = (humanChoice) => {
 
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
 
     const randomNumber = Math.floor(Math.random() * 3)
 
@@ -29,9 +40,9 @@ const playTheGame = (human, machine) => {
 
     if(human === machine){
         result.innerHTML = "Deu empate!"
-    } else if ( (human === 'paper' && machine === 'rock') ||
-                (human === 'rock' && machine === 'scissors') ||
-                (human === 'scissors' && machine === 'paper') ){
+    } else if ( (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+                (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+                (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER) ){
         humanScoreNumber++
         humanScore.innerHTML = humanScoreNumber
         result.innerHTML = "Você ganhou!"
@@ -39,21 +50,21 @@ const playTheGame = (human, machine) => {
         result.innerHTML = "Você perdeu!"
 
         machineScoreNumber++
-        machineScore.innerHTML = humanScoreNumber
+        machineScore.innerHTML = machineScoreNumber
     }
     
     if (machine === 'rock') {
-        rockMachine.style.backgroundColor = "red"
-        scissorsMachine.style.backgroundColor = "black"
-        paperMachine.style.backgroundColor = "black"
+        rockMachine.style.backgroundColor = COLOR_OPTIONS.RED
+        scissorsMachine.style.backgroundColor = COLOR_OPTIONS.BLACK
+        paperMachine.style.backgroundColor = COLOR_OPTIONS.BLACK
     } else if (machine === 'paper') {
-        rockMachine.style.backgroundColor = "black"
-        scissorsMachine.style.backgroundColor = "black"
-        paperMachine.style.backgroundColor = "red"
+        rockMachine.style.backgroundColor = COLOR_OPTIONS.BLACK
+        scissorsMachine.style.backgroundColor = COLOR_OPTIONS.BLACK
+        paperMachine.style.backgroundColor = COLOR_OPTIONS.RED
     } else if (machine === 'scissors') {
-        rockMachine.style.backgroundColor = "black"
-        scissorsMachine.style.backgroundColor = "red"
-        paperMachine.style.backgroundColor = "black"
+        rockMachine.style.backgroundColor = COLOR_OPTIONS.BLACK
+        scissorsMachine.style.backgroundColor = COLOR_OPTIONS.RED
+        paperMachine.style.backgroundColor = COLOR_OPTIONS.BLACK
     }
 }
 
